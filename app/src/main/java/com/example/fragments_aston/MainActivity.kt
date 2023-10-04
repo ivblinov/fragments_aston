6166e628562ff47adec438cf3038757bab21b670
Ivan Blinov <iv2002blinov@gmail.com>
2023-10-04 11:44:50 +0700

    clear code

--- app/src/main/java/com/example/fragments_aston/MainActivity.kt
+++ app/src/main/java/com/example/fragments_aston/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.fragments_aston
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 
@@ -11,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportFragmentManager.commit {
-            replace<FragmentA>(R.id.fragment_container)
+            add<FragmentA>(R.id.fragment_container)
             addToBackStack(FragmentA::class.java.simpleName)
         }
     }
